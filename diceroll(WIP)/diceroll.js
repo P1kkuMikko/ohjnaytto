3baddcdcd8b653878b31f45862f00a916b5c256b
P1kkuMikko <mikko.nyk@gmail.com>
2024-10-28 10:52:21 +0200

    SOme updates to the dice rolling, input, style changes

--- diceroll(WIP)/diceroll.js
+++ diceroll(WIP)/diceroll.js
@@ -36,44 +36,103 @@ function createDice(number) {
   };
 
   const dice = document.createElement("div");
-
   dice.classList.add("dice");
 
   for (const dotPosition of dotPositionMatrix[number]) {
     const dot = document.createElement("div");
-
-    dot.classList.add("dice-dot")
-    dot.style.setProperty("--top", dotPosition[0] + "%")
-    dot.style.setProperty("--left", dotPosition[1] + "%")
-    dice.appendChild(dot)
-
+    dot.classList.add("dice-dot");
+    dot.style.setProperty("--top", dotPosition[0] + "%");
+    dot.style.setProperty("--left", dotPosition[1] + "%");
+    dice.appendChild(dot);
   }
 
   return dice;
 }
 
-function randomizeDice(diceContainer, numberOfDice) {
-  diceContainer.innerHTML = ""
+function updateDice(dice, number) {
+  const dotPositionMatrix = {
+    1: [
+      [50, 50]
+    ],
+    2: [
+      [20, 20],
+      [80, 80]
+    ],
+    3: [
+      [20, 20],
+      [50, 50],
+      [80, 80]
+    ],
+    4: [
+      [20, 20],
+      [20, 80],
+      [80, 20],
+      [80, 80]
+    ],
+    5: [
+      [20, 20],
+      [20, 80],
+      [50, 50],
+      [80, 20],
+      [80, 80]
+    ],
+    6: [
+      [20, 20],
+      [20, 80],
+      [50, 20],
+      [50, 80],
+      [80, 20],
+      [80, 80]
+    ]
+  };
 
-  for (let i = 0; i < numberOfDice; i++) {
-    const random = Math.floor((Math.random() * 6) + 1)
-    const dice = createDice(random)
-
-    diceContainer.appendChild(dice)
-
+  dice.innerHTML = '';
+  for (const dotPosition of dotPositionMatrix[number]) {
+    const dot = document.createElement("div");
+    dot.classList.add("dice-dot");
+    dot.style.setProperty("--top", dotPosition[0] + "%");
+    dot.style.setProperty("--left", dotPosition[1] + "%");
+    dice.appendChild(dot);
   }
 }
 
-const NUMBER_OF_DICE = 5
-const diceContainer = document.querySelector(".dice-container")
-const btnRollDice = document.querySelector(".btn-roll-dice")
+document.addEventListener('DOMContentLoaded', function () {
+  const rollButton = document.querySelector('.btn-roll-dice');
+  rollButton.addEventListener('click', function () {
+    const numberOfDiceInput = document.getElementById('number-of-dice');
+    const numberOfDice = parseInt(numberOfDiceInput.value, 10);
+    const maxDice = parseInt(numberOfDiceInput.max, 10);
 
-randomizeDice(diceContainer, NUMBER_OF_DICE)
+    if (isNaN(numberOfDice) || numberOfDice < 1 || numberOfDice > maxDice) {
+      alert(`Please enter a valid number of dice (1-${maxDice}).`);
+      return;
+    }
 
-btnRollDice.addEventListener("click", () => {
-  const interval = setInterval(() => {
-    randomizeDice(diceContainer, NUMBER_OF_DICE)
-  }, 50)
+    // Clear the dice container
+    const diceContainer = document.querySelector('.dice-container');
+    diceContainer.innerHTML = '';
 
-  setTimeout(() => clearInterval(interval), 1000)
-})
+    // Create initial dice elements
+    const diceElements = [];
+    for (let i = 0; i < numberOfDice; i++) {
+      const dice = createDice(Math.floor(Math.random() * 6) + 1);
+      diceContainer.appendChild(dice);
+      diceElements.push(dice);
+    }
+
+    // Animate the dice roll
+    const interval = setInterval(() => {
+      diceElements.forEach(dice => {
+        updateDice(dice, Math.floor(Math.random() * 6) + 1);
+      });
+    }, 50);
+
+    setTimeout(() => clearInterval(interval), 1000);
+
+    setTimeout(() => {
+      diceElements.forEach(dice => {
+        updateDice(dice, Math.floor(Math.random() * 6) + 1);
+      });
+    }, 1000);
+  });
+});
