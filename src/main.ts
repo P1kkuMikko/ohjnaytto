15ee5c0a11a9d0caf0807d72ac0fe10d6922056d
JoelTurtiainen <joel.turtiainen@gmail.com>
2024-10-29 13:07:12 +0200

    Demo

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,48 @@
-import 'gridstack/dist/gridstack.min.css'
-import './style.css'
-import { GridStack } from 'gridstack'
+import "gridstack/dist/gridstack.css";
+import { GridStack } from "gridstack";
 
-var grid = GridStack.init({
-  cellHeight: "100px",
-  float: true, 
-  resizable: {
-  handles: 'sw'
-},
-columnOpts: {
-        breakpointForWindow: false,  // test window vs grid size
-        breakpoints: [{w:700, c:1},{w:850, c:3},{w:950, c:6},{w:1100, c:8}]
-      },
+// NOTE: REAL apps would sanitize-html or DOMPurify before blinding setting innerHTML. see #2736
+GridStack.renderCB = function (el, w) {
+  el.innerHTML = w.content;
+};
+
+let children = [
+  { x: 0, y: 0, w: 4, h: 2, content: "1" },
+  { x: 4, y: 0, w: 4, h: 4, locked: true, content: 'I can\'t be moved or dragged, nor pushed by others!<br><ion-icon name="ios-lock"></ion-icon>' },
+  {
+    x: 8,
+    y: 0,
+    w: 2,
+    h: 2,
+    minW: 2,
+    noResize: true,
+    content:
+      '<p class="card-text text-center" style="margin-bottom: 0">Drag me!<p class="card-text text-center"style="margin-bottom: 0"><ion-icon name="hand"></ion-icon><p class="card-text text-center" style="margin-bottom: 0">...but don\'t resize me!',
+  },
+  { x: 10, y: 0, w: 2, h: 2, content: "4" },
+  { x: 0, y: 2, w: 2, h: 2, content: "5" },
+  { x: 2, y: 2, w: 2, h: 4, content: "6" },
+  { x: 8, y: 2, w: 4, h: 2, content: "7" },
+  { x: 0, y: 4, w: 2, h: 2, content: "8" },
+  { x: 4, y: 4, w: 4, h: 2, content: "9" },
+  { x: 8, y: 4, w: 2, h: 2, content: "10" },
+  { x: 10, y: 4, w: 2, h: 2, content: "11" },
+];
+let insert = [{ h: 2, content: "new item" }];
+
+let grid = GridStack.init({
+  float: true,
+  cellHeight: 70,
+  acceptWidgets: true,
+  removable: "#trash", // drag-out delete class
+  children,
 });
-grid.addWidget({w: 2, content: 'item 1'});
-grid.addWidget({w: 2, h:2, content: 'item 2'});
-grid.addWidget({w: 2, content: 'item 3'});
+GridStack.setupDragIn(".sidepanel>.grid-stack-item", undefined, insert);
 
+grid.on("added removed change", function (e, items) {
+  let str = "";
+  items.forEach(function (item) {
+    str += " (x,y)=" + item.x + "," + item.y;
+  });
+  console.log(e.type + " " + items.length + " items:" + str);
+});
