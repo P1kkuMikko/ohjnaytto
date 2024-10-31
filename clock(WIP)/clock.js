7017765ea93a7cc60b589647fb57456b266ab8d2
P1kkuMikko <mikko.nyk@gmail.com>
2024-10-31 13:46:29 +0200

    vanilla js version of the clock

--- clock(WIP)/clock.js
+++ clock(WIP)/clock.js
@@ -1,21 +1,23 @@
-var dialLines = document.getElementsByClassName('diallines');
-var clockEl = document.getElementsByClassName('clock')[0];
+document.addEventListener("DOMContentLoaded", function() {
+  var clockEl = document.querySelector('.clock');
 
-for (var i = 1; i < 60; i++) {
-    clockEl.innerHTML += "<div class='diallines'></div>";
-    dialLines[i].style.transform = "rotate(" + 6 * i + "deg)";
-}
+  for (var i = 1; i < 60; i++) {
+    var dialLine = document.createElement('div');
+    dialLine.className = 'diallines';
+    dialLine.style.transform = "rotate(" + 6 * i + "deg)";
+    clockEl.appendChild(dialLine);
+  }
 
-function clock() {
+  function clock() {
     var weekday = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday"
-    ],
+          "Sunday",
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday"
+        ],
         d = new Date(),
         h = d.getHours(),
         m = d.getMinutes(),
@@ -23,28 +25,30 @@ function clock() {
         date = d.getDate(),
         month = d.getMonth() + 1,
         year = d.getFullYear(),
-
-        hDeg = h * 30 + m * (360 / 720),
-        mDeg = m * 6 + s * (360 / 3600),
+             
+        hDeg = h * 30 + m * (360/720),
+        mDeg = m * 6 + s * (360/3600),
         sDeg = s * 6,
-
-        hEl = document.querySelector('.minute-hand'),
-        mEl = document.querySelector('.hour-hand'),
+        
+        hEl = document.querySelector('.hour-hand'),
+        mEl = document.querySelector('.minute-hand'),
         sEl = document.querySelector('.second-hand'),
         dateEl = document.querySelector('.date'),
         dayEl = document.querySelector('.day');
-
+    
     var day = weekday[d.getDay()];
-
-    if (month < 9) {
-        month = "0" + month;
+    
+    if(month < 10) {
+      month = "0" + month;
     }
-
-    hEl.style.transform = "rotate(" + hDeg + "deg)";
-    mEl.style.transform = "rotate(" + mDeg + "deg)";
-    sEl.style.transform = "rotate(" + sDeg + "deg)";
+    
+    hEl.style.transform = "rotate("+hDeg+"deg)";
+    mEl.style.transform = "rotate("+mDeg+"deg)";
+    sEl.style.transform = "rotate("+sDeg+"deg)";
     dateEl.innerHTML = date + "/" + month + "/" + year;
     dayEl.innerHTML = day;
-}
+  }
 
-setInterval("clock()", 100);
+  setInterval(clock, 1000);
+  clock(); // Initial call to set the clock immediately
+});
